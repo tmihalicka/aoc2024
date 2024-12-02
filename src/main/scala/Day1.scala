98a59960f976bc0cffb297547a5838fcf0aaef69
Tomas Mihalicka <tomas@mihalicka.com>
2024-12-02 12:59:52 +0100

    Advent of Code 2024 - Day 2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -4,38 +4,34 @@ package com.mihalicka.aoc2024
  * Day 1: Historian Hysteria
  * @see https://adventofcode.com/2024/day/1
  */
-object Day1:
+object Day1 extends AoC2024(1):
 
   private def inputLines: List[(Int, Int)] =
-    ResourceLoader
-      .loadResource(1)
+    puzzleInput
       .map(_.trim.split("\\s+").map(_.toInt))
       .map { case Array(left, right) => (left, right) }
 
-  private def solutionPart1: Int =
+  private def part1(): Unit =
     val (left, right) = inputLines.unzip
 
-    left
+    val result = left
       .sorted
       .zip(right.sorted)
       .map((left, right) => math.abs(left - right))
       .sum
 
-  private def solutionPar2: Int =
+    println(s"Total distance: $result")
+
+  private def part2(): Unit =
     val (left, right) = inputLines.unzip
 
     val rightFrequency = right.groupBy(identity).view.mapValues(_.size).toMap
 
-    left
+    val result = left
       .map(num => num * rightFrequency.getOrElse(num, 0)).sum
 
+    println(s"Similarity Score: $result")
 
-  @main def run() =
-    println(s"Total distance: $solutionPart1")
-    println(s"Similarity Score: $solutionPar2")
-
-
-
-
-
-
+  @main def runDay1(): Unit =
+    part1()
+    part2()
---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,37 @@
+package com.mihalicka.aoc2024
+
+/**
+ * Day 2: Red-Nosed Reports
+ * @see https://adventofcode.com/2024/day/2
+ */
+object Day2 extends AoC2024(2):
+
+  private def loadLines: List[List[Int]] =
+    puzzleInput
+      .map(_.split(" ").map(_.toInt).toList)
+
+  private def isSafe(input: List[Int]): Boolean =
+    val diff = input.sliding(2).map { case Seq(a, b) => a - b }.toList
+    diff.forall(d => d >= 1 && d <= 3) || diff.forall(d => d <= -1 && d >= -3)
+
+  private def part1(): Unit =
+    val safeReports = loadLines.count(isSafe)
+    println("Safe reports: " + safeReports)
+
+  private def part2(): Unit =
+    def isReportSafeWithDampener(report: List[Int]): Boolean =
+      report.indices.exists { i =>
+        val modified = report.take(i) ++ report.drop(i + 1)
+        isSafe(modified)
+      }
+
+    def safeDempeneredReports = loadLines.count { report =>
+      isSafe(report) || isReportSafeWithDampener(report)
+    }
+
+    println("Dampenered safe reports: " + safeDempeneredReports)
+
+
+  @main def runDay2(): Unit =
+    part1()
+    part2()
